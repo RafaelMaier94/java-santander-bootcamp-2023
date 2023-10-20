9df51109be38cf1080bbdfc62b7a9bcdc24e39e5
rafael.maier <rafael.maier@eemovel.com>
2023-10-20 11:27:31 -0300

    feat: user service

--- src/main/java/me/dio/santanderbootcamp2023/domain/repository/UserRepository.java
+++ src/main/java/me/dio/santanderbootcamp2023/domain/repository/UserRepository.java
@@ -7,5 +7,5 @@ import me.dio.santanderbootcamp2023.domain.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    boolean existsByAccountNumber(String accountNumber);
 }
---
+++ src/main/java/me/dio/santanderbootcamp2023/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package me.dio.santanderbootcamp2023.service.impl;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import me.dio.santanderbootcamp2023.domain.model.User;
+import me.dio.santanderbootcamp2023.domain.repository.UserRepository;
+import me.dio.santanderbootcamp2023.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This account number already exists");
+        }
+
+        return userRepository.save(userToCreate);
+    }
+
+}
